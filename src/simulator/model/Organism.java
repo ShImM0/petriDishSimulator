cad35608011ff9e271d63b96e5c6644012ff613a
ShImM0 <80552567+ShImM0@users.noreply.github.com>
2026-08-01 23:55:57 +0200

    Add Organism attributes

--- src/simulator/model/Organism.java
+++ src/simulator/model/Organism.java
@@ -3,75 +3,76 @@ package simulator.model;
 import java.awt.Color;
 
 public class Organism implements OrganismInfo {
-	
-	public enum State{
+
+	public enum State {
 		HEALTHY, HUNGRY, RECOVERING
-	};
+	}
+
+	private String id;
+	private State state;
+	private Vector2D pos;
+	private Color color;
+	private long age;
+	private double speed;
+	private double size;
+	private double sight;
+	private double strength;
+	private double energy;
+	private boolean alive;
+
+	@Override
+	public State getState() {
+		return this.state;
+	}
 
 	@Override
 	public Vector2D getPosition() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.pos;
 	}
 
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public long getAge() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getSpeed() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getSight() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getStrength() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getEnergy() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isAlive() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public State getState() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.id;
 	}
 
 	@Override
 	public Color getColor() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.color;
 	}
+
+	@Override
+	public long getAge() {
+		return this.age;
+	}
+
+	@Override
+	public double getSpeed() {
+		return this.speed;
+	}
+
+	@Override
+	public double getSize() {
+		return this.size;
+	}
+
+	@Override
+	public double getSight() {
+		return this.sight;
+	}
+
+	@Override
+	public double getStrength() {
+		return this.strength;
+	}
+
+	@Override
+	public double getEnergy() {
+		return this.energy;
+	}
+
+	@Override
+	public boolean isAlive() {
+		return this.alive;
+	};
 
 }
