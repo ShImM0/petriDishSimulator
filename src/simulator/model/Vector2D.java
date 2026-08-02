a1dd81f3e1dc0d0fbc877cae15754e4fc3d84a56
ShImM0 <80552567+ShImM0@users.noreply.github.com>
2026-08-02 23:57:26 +0200

    Update Vector2D.java

--- src/simulator/model/Vector2D.java
+++ src/simulator/model/Vector2D.java
@@ -50,9 +50,44 @@ public class Vector2D {
 		return subtract(other).magnitude();
 	}
 
+	public Vector2D direction() {
+		if (magnitude() > 0.0) {
+			return scale(1.0 / magnitude());
+		} else
+			return new Vector2D(this);
+	}
+
 	@Override
 	public String toString() {
 		return String.format("[%.2f, %.2f]", this.x, this.y);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vector2D other = (Vector2D) obj;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+			return false;
+		return true;
 	}
 
 }
